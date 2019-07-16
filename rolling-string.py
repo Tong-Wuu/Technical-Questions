89a5b3fee3e44998539e9f2df9eebfd6480611c2
TongWu-UW <tong.wupersonal@gmail.com>
2019-07-15 20:04:26 -0400

    rolling-string solution 2

--- rolling-string.py
+++ rolling-string.py
@@ -35,5 +35,31 @@ def solution_2_roll(s, roll):
     return ''.join(chr(char) for char in ordChar)
 
 
-print(solution_1_roll("abc", [3, 3, 3, 3, 3, 2]))
-print(solution_2_roll("abc", [3, 3, 3, 3, 3, 2]))
+def solution_3_roll(s, roll):
+    # 1) map each character to ord
+    # 2) for each roll, add one to substring range
+    # 3) map again to check the index
+    # 4) map integers back to chr with index
+
+    def get_sum_each_roll(roll):
+        initSumOfEachRoll = [0] * len(s)
+        for ind in range(0, roll):
+            initSumOfEachRoll[ind] += 1
+        return initSumOfEachRoll
+
+    def do_sum_lists(lst_1, lst_2):
+        return [sum(x) for x in zip(lst_1, lst_2)]
+
+    def check_position(num):
+        a_to_z = [chr(i) for i in range(97, 123)]
+        return a_to_z[(num - 97) % 26]
+
+    listOfChar = map(lambda x: x, s)  # convert string into list of char
+    ordChar = map(lambda x: ord(x), listOfChar)  # convert each char to ord int
+    eachRoll = map(get_sum_each_roll, roll)  # for each roll find the substring chars
+    from functools import reduce
+    sumOfEachRoll = reduce(do_sum_lists, eachRoll)  # reduce to find the sum
+    rolled = [sum(x) for x in zip(ordChar, sumOfEachRoll)]  # sum total rolled number for each char
+    check = map(check_position, rolled)  # check and turn back the char
+
+    return ''.join(check)  # join the characters
